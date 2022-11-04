3fc08d02c0717bee9a641f608d74107ef73efcc5
Bobbbay <abatterysingle@gmail.com>
2022-11-04 02:32:45 +0000

    feat: expanded motor type

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+use arfur::prelude::*;
 use mekena::prelude::*;
 use miette::{IntoDiagnostic, Result};
 
 #[main]
 async fn main(system: System) -> Result<()> {
+    let robot = RobotBuilder::default().initialize()?;
+
     system
-        .add_node(subsystems::drivetrain::Drivetrain::new())
+        .add_node(subsystems::drivetrain::Drivetrain::new(robot))
         .start()
         .await
         .into_diagnostic()
@@ -12,6 +15,8 @@ async fn main(system: System) -> Result<()> {
 
 pub mod subsystems {
     pub mod drivetrain {
+
+        use arfur::prelude::*;
         use mekena::prelude::*;
 
         pub struct Drivetrain {
@@ -20,10 +25,28 @@ pub mod subsystems {
         }
 
         impl Drivetrain {
-            pub fn new() -> Self {
+            pub fn new(robot: Robot) -> Self {
                 Self {
-                    motors: Motors::new(),
+                    motors: Motors::new(robot, (10, 11), (12, 13), (14, 15), (16, 17)),
                     state: State::Off,
+                }
+            }
+
+            /// TODO: move this to Arfur.
+            /// TODO: review speed/benchmark and optimise
+            fn deadband(x: f32) -> f32 {
+                const CUTOFF: f32 = 0.1;
+                const WEIGHT: f32 = 0.2;
+
+                fn cubic(x: f32, weight: f32) -> f32 {
+                    weight * x * x * x + (1. - weight) * x
+                }
+
+                if x.abs() < CUTOFF {
+                    0
+                } else {
+                    cubic(x, WEIGHT)
+                        - (x.abs() / x) * cubic(CUTOFF, WEIGHT) / (1. - cubic(CUTOFF, WEIGHT))
                 }
             }
         }
@@ -35,8 +58,18 @@ pub mod subsystems {
 
                 loop {
                     // Get joystick values.
+                    let x = 0.;
+                    let y = 0.;
+                    let θ = 0.;
+
                     // Apply transformations.
+                    let x = Self::deadband(x);
+                    let y = Self::deadband(y);
+                    let θ = Self::deadband(θ);
+
                     // Run the applied transforms on self.motors
+
+                    // Sleep, and do it all again.
                     time::sleep(Duration::from_millis(50)).await;
                 }
             }
@@ -44,11 +77,41 @@ pub mod subsystems {
 
         /// Motor handles to all eight motors. Can possibly be one handle to a
         /// Swerve type in Arfur.
-        pub struct Motors {}
+        pub struct Motors {
+            lf: Module,
+            lr: Module,
+            rf: Module,
+            rr: Module,
+        }
 
         impl Motors {
-            pub fn new() -> Self {
-                Self {}
+            pub fn new(
+                robot: Robot,
+                lf_ids: (i32, i32),
+                lr_ids: (i32, i32),
+                rf_ids: (i32, i32),
+                rr_ids: (i32, i32),
+            ) -> Self {
+                Self {
+                    lf: Module::new(robot, lf_ids.0, lf_ids.1),
+                    lr: Module::new(robot, lr_ids.0, lr_ids.1),
+                    rf: Module::new(robot, rf_ids.0, rf_ids.1),
+                    rr: Module::new(robot, rr_ids.0, rr_ids.1),
+                }
+            }
+        }
+
+        pub struct Module {
+            rotation: SparkMax,
+            drive: SparkMax,
+        }
+
+        impl Module {
+            pub fn new(robot: Robot, rotation_id: i32, drive_id: i32) -> Self {
+                Self {
+                    rotation: SparkMax::new(robot, rotation_id),
+                    drive: SparkMax::new(robot, drive_id),
+                }
             }
         }
 
